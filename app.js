ecab6be9c37bf297c6271c612b45e87d3a364edb
Céline Gourc <celine.gourc1@laposte.net>
2023-05-12 14:22:24 +0200

    mise en place de .env

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
 const bookRoutes = require('./routes/books')
-const path = require('path');
+const path = require('path')
+const dotenv = require('dotenv').config()
 
-mongoose.connect('mongodb+srv://celinegourc:OpenClassRooms@cluster1.o21r8wy.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGO_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 const app = express();
 
