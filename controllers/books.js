a4ca022f2c0f6d8a531b52ace0721c85b8ca9d49
Céline Gourc <celine.gourc1@laposte.net>
2023-05-25 15:29:02 +0200

    Ajout de la vérif de vote antérieur de l'utilisateur pour le livre

--- controllers/books.js
+++ controllers/books.js
@@ -3,16 +3,17 @@ const fs = require('fs')
 
 exports.postBook = async (req, res) => {
   try {
-    const bookObject = JSON.parse(req.body.book);
-   
+    const bookObject = JSON.parse(req.body.book)
+
     const today = new Date();
-    const year = today.getFullYear();
+    const year = today.getFullYear()
+       // Si l'année de publication du livre est supérieure à l'année actuelle, renvoie une erreur et un message correspondant
     if (bookObject.year > year) {
-      console.log("Année de publication postérieure de la date actuelle");
-      res.status(400).json("Année de publication postérieure de la date actuelle.");
+      console.log("Année de publication postérieure de la date actuelle")
+      res.status(400).json("Année de publication postérieure de la date actuelle.")
     } else {
-      delete bookObject._id;
-      delete bookObject._userId;
+      delete bookObject._id
+      delete bookObject._userId
 
       const book = new Book({
         ...bookObject,
@@ -20,16 +21,14 @@ exports.postBook = async (req, res) => {
         imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename.split('.')[0]}resized.webp`
       });
 
-      await book.save();
+      await book.save()
 
-      res.status(201).json({ message: 'Livre enregistré !' });
+      res.status(201).json({ message: 'Livre enregistré !' })
     }
   } catch (error) {
     res.status(400).json({ error })
   }
 }
-
-
 
 exports.getAllBooks = async (req, res) => {
   try {
@@ -45,46 +44,54 @@ exports.getOneBook = async (req,res)=>{
     const book = await  Book.findOne({ _id: req.params.id })
     res.status(200).json(book)
   } catch (error) { 
-    res.status(404).json({ error });
+    res.status(404).json({ error })
   }
 }
 
 exports.modifyBook = async (req, res) => {
   try {
+    // Vérifie si une image est attachée à la requête et construit l'objet book en conséquence
     const bookObject = req.file ? { 
       ...JSON.parse(req.body.book),
       imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename.split('.')[0]}resized.webp`
-    } : {...req.body} 
+    } : {...req.body}
+    
     const today = new Date();
-    const year = today.getFullYear();
+    const year = today.getFullYear()
+    
     if (bookObject.year > year) {
-      console.log("Année de publication postérieure de la date actuelle");
-      res.status(400).json("Année de publication postérieure de la date actuelle.");
+      // Si l'année de publication du livre est supérieure à l'année actuelle, renvoie une erreur et un message correspondant
+      console.log("Année de publication postérieure à la date actuelle");
+      res.status(400).json("Année de publication postérieure à la date actuelle.")
     } else {
-    delete bookObject._userId;
-    const book = await Book.findOne({_id: req.params.id})
-    if (book.userId != req.auth.userId) {
-      res.status(401).json({ message : 'Not authorized'})
-    } else {
-      if (req.file) {
-       try {
-        const filename = book.imageUrl.split('/images/')[1];
-          fs.unlink(`images/${filename}`, (err) => {
-            if (err) throw err
-          });
-        } catch (error) {
-          console.log(error);
-          res.status(500).json({ error })
-        }
-       
+      delete bookObject._userId
+      const book = await Book.findOne({_id: req.params.id})
+      
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message : 'Not authorized'})
+      } else {
+        if (req.file) {
+          try {
+            const filename = book.imageUrl.split('/images/')[1]
+            fs.unlink(`images/${filename}`, (err) => {
+              if (err) throw err
+            });
+          } catch (error) {
+            console.log(error);
+            res.status(500).json({ error })
+          }
+        }       
+        // Met à jour le livre correspondant à l'identifiant fourni avec les nouvelles informations de bookObject
+        await Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+  
+        res.status(200).json({message : 'Objet modifié!'})
       }
-      await Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
-      res.status(200).json({message : 'Objet modifié!'})
-    } }
+    }
   } catch(error) {
     res.status(400).json({ error })
   }
 }
+
 
 exports.deleteBook = async (req, res) => {
   try {
@@ -103,8 +110,6 @@ exports.deleteBook = async (req, res) => {
   }
 }
 
-
-
 exports.rateBook = async (req,res)=>{
   try {
     const book = await Book.findOne({_id: req.params.id})
@@ -112,8 +117,10 @@ exports.rateBook = async (req,res)=>{
       userId: req.auth.userId,
       grade: req.body.rating
     }
+
+    const hasUserVoted = book.ratings.find(rating => rating.userId === req.auth.userId)
     
-    if (book.userId != req.auth.userId){
+    if ((book.userId !== req.auth.userId) && (!hasUserVoted)){
       book.ratings.push(newRatingObject) 
       const allRatings = book.ratings.map(rating => rating.grade)
       const averageRating = allRatings.reduce((a, b) => a + b, 0) / allRatings.length
@@ -121,9 +128,12 @@ exports.rateBook = async (req,res)=>{
       book.averageRating = averageRating.toFixed(1)
 
       await book.save()
-    }
+      res.status(200).json(book)
 
-    res.status(200).json(book)
+    } else { 
+      await res.status(401).json({ error: 'Vous ne pouvez pas voter pour ce livre.'})
+    }
+    
   } catch (error) {
     res.status(401).json( { error })
   }
--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('../middleware/multer-config')
-const resizedImage = require('../middleware/sharp-config');
+const resizedImage = require('../middleware/sharp-config')
 const auth = require('../middleware/auth')
 
-const bookCtrl = require('../controllers/books');
+const bookCtrl = require('../controllers/books')
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getBestBooks )
@@ -16,4 +16,4 @@ router.delete('/:id', auth, bookCtrl.deleteBook)
 
 
 
-module.exports = router;
+module.exports = router
