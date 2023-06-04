db2802cd6e927724a02a2961db1f21541a8555b6
Céline Gourc <celine.gourc1@laposte.net>
2023-06-04 09:54:08 +0200

    ajout du min max de la note dans le modèle book

--- models/book.js
+++ models/book.js
@@ -9,11 +9,21 @@ const bookSchema = mongoose.Schema({
     genre: { type: String, required: true },
     ratings :[
         {
-            userId: { type: String, required: true },
-            grade: {type: Number, required: true}
+            userId: { 
+                type: String,
+                required: true 
+            },
+            grade: {
+                type: Number,
+                min:1,
+                max:5,  
+                required: true
+            }
         }
     ],
-    averageRating:{type:Number, required:true}
+    averageRating:{
+        type:Number, 
+        required:true}
 })
 
 module.exports = mongoose.model('Book', bookSchema)
