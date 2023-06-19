a43efc6f17a52e2b39d8c37f06d469d77e1dbde8
Céline Gourc <celine.gourc1@laposte.net>
2023-06-19 15:31:13 +0200

    erreur de frappe router.post

--- routes/books.js
+++ routes/books.js
@@ -12,7 +12,7 @@ router.get('/bestrating', bookCtrl.getBestBooks )
 router.get('/:id', bookCtrl.getOneBook)
 router.post('/:id/rating', auth, bookCtrl.rateBook)
 router.post('/', auth, multer, bookCtrl.postBook)
-router.put('/:id', auth, multer, ookCtrl.modifyBook)
+router.put('/:id', auth, multer, bookCtrl.modifyBook)
 router.delete('/:id', auth, bookCtrl.deleteBook)
 
 
